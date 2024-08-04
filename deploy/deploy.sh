fa55a662047b28567ac3f98190e64e8ee4839271
TomJB1 <tomjbrandis@gmail.com>
2024-08-03 19:11:09 -0600

    Check that changes are commited when deploying

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,8 +6,13 @@ echo "$JEKYLL_DIR"
 SITE_DIR=$JEKYLL_DIR/_site
 echo $SITE_DIR
 
+if [[ `git status --porcelain` ]]; then
+echo "commit changes"
+else
 jekyll build -s $JEKYLL_DIR -d $SITE_DIR
 
 ssh root@tombrandis.uk.to < $SCRIPT_DIR/delete_folder.sh
 
 scp -r $JEKYLL_DIR/_site root@tombrandis.uk.to:/var/www/my-website-jekyll-built
+
+fi
