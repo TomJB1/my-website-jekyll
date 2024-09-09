637bc649cec1223d1ce73c28688ffc638dbf9bc5
Tom <tombrandisicloud@gmail.com>
2024-09-09 18:08:49 +0100

    add no-adblock detector
    and encourage them to get one!

---
+++ cmbdv2.js
@@ -0,0 +1,4 @@
+console.log("use a adblocker stupid");
+div=document.createElement("div");
+div.innerText = "I notice you don't use a adblocker. I recommend UBlock Origin (sorry if you got this message in error)"
+document.body.prepend(div);
