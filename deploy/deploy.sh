9f131733de406dd14e0c3ab932cfeb5fc8c82782
TomJB1-obsidian <tomjbrandis@gmail.com>
2024-12-20 10:55:43 +0000

    add server deploy option

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,6 +13,7 @@ while [[ "$#" -gt 0 ]]; do
         -e|--existing) existing=1 ;;
         -l|--local) local=1 ;;
         -m|--maxify) maxify=1 ;;
+        -sd|--server-deploy) server_deploy=1 ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -33,7 +34,11 @@ else
     fi
 
     if [ -n "$clean" ] && [ -z "$local" ]; then
-        ssh root@tombrandis.uk < $SCRIPT_DIR/on_server/delete_folder.sh
+        if [ -n "$server_deploy" ]; then
+            $SCRIPT_DIR/on_server/delete_folder.sh
+        else
+            ssh root@tombrandis.uk < $SCRIPT_DIR/on_server/delete_folder.sh
+        fi
     fi
 
     if [ -z "$local" ]; then
@@ -42,10 +47,18 @@ else
             rsync -r -v -e "/usr/bin/ssh -o UserKnownHostsFile=~/.ssh/known_hosts" /cygdrive/$JEKYLL_DIR/_site/ root@tombrandis.uk:/var/www/my-website-jekyll-built
             unset MSYS_NO_PATHCONV 
         else
-            rsync -r -v $JEKYLL_DIR/_site/ root@tombrandis.uk:/var/www/my-website-jekyll-built
+            if [ -n "$server_deploy" ]; then
+                rsync -r -v $JEKYLL_DIR/_site/ /var/www/my-website-jekyll-built
+            else
+                rsync -r -v $JEKYLL_DIR/_site/ root@tombrandis.uk:/var/www/my-website-jekyll-built
+            fi
         fi
 
-        ssh root@tombrandis.uk < $SCRIPT_DIR/on_server/ownership.sh
+        if [ -n "$server_deploy" ]; then
+            $SCRIPT_DIR/on_server/ownership.sh
+        else
+            ssh root@tombrandis.uk < $SCRIPT_DIR/on_server/ownership.sh
+        fi
     else
         echo "local build - not publishing to server"
     fi
