f9d64b7b75639b01b83c9961fd46278919ea9b62
TomJB1-obsidian <tomjbrandis@gmail.com>
2025-03-28 20:36:03 +0000

    fix bug that overwrote links descriptions

--- deploy/links/add_link.py
+++ deploy/links/add_link.py
@@ -5,7 +5,7 @@ import sys
 
 # add link
 
-desc = sys.argv[3] if (sys.argv[3]=='""') else "A link from Tom Brandis"
+desc = "A link from Tom Brandis" if (sys.argv[3]=='""') else sys.argv[3]
 line = f'\n"{sys.argv[1]}","{sys.argv[2]}","{desc}","{datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M")} GMT"'
 
 with open(str(pathlib.Path(__file__).resolve().parents[2])+"/links/links.csv", "a") as file:
