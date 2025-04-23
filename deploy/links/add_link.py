aa9308e6536dd853df4e6e13ea7e13c47866510d
TomJB1-obsidian <tomjbrandis@gmail.com>
2025-04-23 21:48:59 +0100

    add newline to end of links csv

--- deploy/links/add_link.py
+++ deploy/links/add_link.py
@@ -3,7 +3,7 @@ import pathlib
 import sys
 
 desc = "A link from Tom Brandis" if (sys.argv[3]=='""') else sys.argv[3]
-line = f'\n"{sys.argv[1]}","{sys.argv[2]}","{desc}","{datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M")} GMT"'
+line = f'"{sys.argv[1]}","{sys.argv[2]}","{desc}","{datetime.now(timezone.utc).strftime("%a, %d %b %Y %H:%M")} GMT"\n'
 
 with open(str(pathlib.Path(__file__).resolve().parents[2])+"/links/links.csv", "a") as file:
     file.write(line)
