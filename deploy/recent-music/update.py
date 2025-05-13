4709a0fd0eb753e2f427af8cf08ed3ef4265e4cf
TomJB1-obsidian <tomjbrandis@gmail.com>
2025-05-13 17:24:32 +0100

    add script for top music of the week

---
+++ deploy/recent-music/update.py
@@ -0,0 +1,15 @@
+import requests
+import re
+import os
+
+r = requests.get('https://libre.fm/2.0/?method=user.gettopartists&user=TomBrandis&period=7day&limit=5&format=json')
+if(r.status_code == 200):
+    list_items=""
+    for artist in r.json()["topartists"]["artist"]:
+        list_items+=f"<li>{artist['name']}</li>"
+path = os.path.dirname(os.path.realpath(__file__))+"/../../_site/music/index.html"
+
+with open(path, "r") as music:
+    new_html = re.sub(r'<ol id="music-last-week">.*</ol>', f"<ol id='music-last-week'>{list_items}</ol>", music.read())
+with open(path, "w") as music:
+    music.write(new_html)
